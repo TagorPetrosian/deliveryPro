fba57deb7aa7d3872f2183f84da104916e58b7a3
Tagor Petrosian <p.tagor@gmail.com>
2018-08-17 23:40:14 +0300

    Tweak google strategy settings

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,7 +19,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
