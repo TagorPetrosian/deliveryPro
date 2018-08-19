1bd21ffaa08fdc370cfa4846fb8f59dcf84a6d74
Tagor Petrosian <p.tagor@gmail.com>
2018-08-19 16:51:13 +0300

    Refactored passport using async await instead of promises

--- server/services/passport.js
+++ server/services/passport.js
@@ -22,16 +22,13 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
