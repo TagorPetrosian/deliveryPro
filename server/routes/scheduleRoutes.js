88994f6cbb90ab47efeae7f7d055358d12596a7c
Shiran Salem <p.tagor@gmail.com>
2018-10-05 17:22:56 +0300

    add try catch blocks

--- server/routes/scheduleRoutes.js
+++ server/routes/scheduleRoutes.js
@@ -7,96 +7,104 @@ const Truck = mongoose.model("Truck");
 
 module.exports = app => {
   app.get("/admin/api/schedules", requireLogin, async (req, res) => {
-    const pickups = await Order.find({ recieved: false });
-    const drops = await Order.find({ recieved: true, delivered: false });
-    const zones = await Zone.find({});
-    const trucks = await Truck.find({});
-    const drivers = await User.find({ role: "driver" });
-    const stops = [];
-    const schedules = [];
+    try {
+      const pickups = await Order.find({ recieved: false });
+      const drops = await Order.find({ recieved: true, delivered: false });
+      const zones = await Zone.find({});
+      const trucks = await Truck.find({});
+      const drivers = await User.find({ role: "driver" });
+      const stops = [];
+      const schedules = [];
 
-    //sort to stops by origin
-    for (pickup of pickups) {
-      stops.push({
-        location: pickup.origin,
-        description: pickup.description,
-        _order: pickup.id,
-        volume: pickup.volume,
-        sort: "Pickup"
-      });
-    }
+      //sort to stops by origin
+      for (pickup of pickups) {
+        stops.push({
+          location: pickup.origin,
+          description: pickup.description,
+          _order: pickup.id,
+          volume: pickup.volume,
+          sort: "Pickup"
+        });
+      }
 
-    //sort to stops by destination
-    for (drop of drops) {
-      stops.push({
-        location: drop.destination,
-        description: drop.description,
-        _order: drop.id,
-        volume: drop.volume,
-        sort: "Drop"
-      });
+      //sort to stops by destination
+      for (drop of drops) {
+        stops.push({
+          location: drop.destination,
+          description: drop.description,
+          _order: drop.id,
+          volume: drop.volume,
+          sort: "Drop"
+        });
+      }
+      const deviderSize = Math.floor(
+        stops.length / Math.min(drivers.length, trucks.length)
+      );
+      while (stops.length > 0) {
+        let driver = drivers.pop();
+        schedules.push({
+          _driver: driver.id,
+          driverName: driver.fullName,
+          stops: stops.splice(0, deviderSize)
+        });
+      }
+      res.send(schedules);
+    } catch (err) {
+      res.status(422).send(err);
     }
-    const deviderSize = Math.floor(
-      stops.length / Math.min(drivers.length, trucks.length)
-    );
-    while (stops.length > 0) {
-      let driver = drivers.pop();
-      schedules.push({
-        _driver: driver.id,
-        driverName: driver.fullName,
-        stops: stops.splice(0, deviderSize)
-      });
-    }
-    res.send(schedules);
   });
 
   app.get("/driver/api/schedule", requireLogin, async (req, res) => {
-    const pickups = await Order.find({ recieved: false });
-    const drops = await Order.find({ recieved: true, delivered: false });
-    const zones = await Zone.find({});
-    const trucks = await Truck.find({});
-    const drivers = await User.find({ role: "driver" });
-    const stops = [];
-    const schedules = [];
+    try {
+      const pickups = await Order.find({ recieved: false });
+      const drops = await Order.find({ recieved: true, delivered: false });
+      const zones = await Zone.find({});
+      const trucks = await Truck.find({});
+      const drivers = await User.find({ role: "driver" });
+      const stops = [];
+      const schedules = [];
 
-    //sort to stops by origin
-    for (pickup of pickups) {
-      stops.push({
-        location: pickup.origin,
-        description: pickup.description,
-        _order: pickup.id,
-        volume: pickup.volume,
-        sort: "Pickup"
-      });
-    }
-
-    //sort to stops by destination
-    for (drop of drops) {
-      stops.push({
-        location: drop.destination,
-        description: drop.description,
-        _order: drop.id,
-        volume: drop.volume,
-        sort: "Drop"
-      });
-    }
-    const deviderSize = Math.floor(
-      stops.length / Math.min(drivers.length, trucks.length)
-    );
-
-    while (stops.length > 0) {
-      let driver = drivers.pop();
-      schedules.push({
-        _driver: driver.id,
-        driverName: driver.fullName,
-        stops: stops.splice(0, deviderSize)
-      });
-    }
-
-    for (schedule of schedules) {
-      if (schedule._driver === req.user.id) {
-        res.send(schedule);
+      //sort to stops by origin
+      for (pickup of pickups) {
+        stops.push({
+          location: pickup.origin,
+          description: pickup.description,
+          _order: pickup.id,
+          volume: pickup.volume,
+          sort: "Pickup"
+        });
       }
+
+      //sort to stops by destination
+      for (drop of drops) {
+        stops.push({
+          location: drop.destination,
+          description: drop.description,
+          _order: drop.id,
+          volume: drop.volume,
+          sort: "Drop"
+        });
+      }
+      const deviderSize = Math.floor(
+        stops.length / Math.min(drivers.length, trucks.length)
+      );
+
+      while (stops.length > 0) {
+        let driver = drivers.pop();
+        schedules.push({
+          _driver: driver.id,
+          driverName: driver.fullName,
+          stops: stops.splice(0, deviderSize)
+        });
+      }
+
+      for (schedule of schedules) {
+        if (schedule._driver === req.user.id) {
+          res.send(schedule);
+        }
+      }
+    } catch (err) {
+      res.status(422).send(err);
     }
   });
 };
